8f601942e5a59b3f139cdd871d5b7fbb8f16f850
Trang Vu <106600297+Trangvu801@users.noreply.github.com>
2024-12-29 18:33:32 -0500

    AboutSection completed

--- starter-template/src/sections/About.tsx
+++ starter-template/src/sections/About.tsx
@@ -12,6 +12,8 @@ import GithubIcon from '@/assets/icons/github.svg';
 import { TechIcon } from "@/components/TechIcon";
 import mapImage from "@/assets/images/map.png"
 import simleMemoji from "@/assets/images/memoji-smile.png"
+import { CardHeader } from "@/components/CardHeader";
+import { ToolboxItems } from "@/components/ToolboxItems";
 
 const toolboxItems = [
   {
@@ -42,79 +44,127 @@ const toolboxItems = [
 
 const hobbies = [
   {
-    title:'Meditation',
-    emoji:'🧘🏻',
+    title: 'Meditation',
+    emoji: '🧘🏻',
+    left: '5%',
+    top: '5%',
   },
   {
-    title:'Photography',
-    emoji:'📷',
+    title: 'Photography',
+    emoji: '📷',
+    left: '50%',
+    top: '5%',
   },
   {
-    title:'Cooking',
-    emoji:'🧑🏻‍🍳',
+    title: 'Fitness',
+    emoji: '🏋🏻',
+    left: '35%',
+    top: '40%',
   },
   {
-    title:'Fitness',
-    emoji:'🏋🏻',
+    title: 'Cooking',
+    emoji: '🧑🏻‍🍳',
+    left: '10%',
+    top: '35%',
   },
   {
-    title:'Reading',
-    emoji:'📚',
+    title: 'Reading',
+    emoji: '📚',
+    left: '70%',
+    top: '45%',
   },
   {
-    title:'Film',
-    emoji:'🎞️',
+    title: 'Film',
+    emoji: '🎞️',
+    left: '5%',
+    top: '65%',
   },
   {
-    title:'Journal',
-    emoji:'✍🏻',
+    title: 'Journal',
+    emoji: '✍🏻',
+    left: '45%',
+    top: '70%',
   },
 ]
 
 export const AboutSection = () => {
   return (
-    <div className="pb-96">
-      <SectionHeader
-        eyebrow="About Me"
-        title="A Glimpse Into My World"
-        description="Learn more about who I am, what I do, and what inspires me."
-      />
-      <div>
-        <Card>
-          <div>
-            <StarIcon />
-            <h3>My Reads</h3>
-            <p>Explore the book shaping my perspective.</p>
-          </div>
-          <Image src={bookImage} alt="Book cover" />
-        </Card>
-        <Card>
-          <div>
-            <StarIcon />
-            <h3>My Toolbox</h3>
-            <p>Explore the technologies and tools I use to craft exceptional 
-              digital experiences.</p>
-          </div>
-          <div>
-            {toolboxItems.map(item => (
-              <div key={item.title}>
-                <TechIcon component={item.iconType} />
-                <span>{item.title}</span>
+    <div className="py-20 lg:py-28">
+      <div className="container">
+        <SectionHeader
+          eyebrow="About Me"
+          title="A Glimpse Into My World"
+          description="Learn more about who I am, what I do, and what inspires me."
+        />
+        <div className="mt-20 flex flex-col gap-8">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+            <Card className="h-[320px] md:col-span-2 lg:col-span-1">
+              <CardHeader
+                title="My Reads"
+                description="Explore the book shaping my perspective."
+              />
+              <div className="w-40 mx-auto mt-2 md:mt-0">
+                <Image src={bookImage} alt="Book cover" />
               </div>
-            ))}
+            </Card>
+            <Card className="h-[320px] md:col-span-3 lg:col-span-2">
+              <CardHeader
+                title="My Toolbox"
+                description="Explore the technologies and tools I use to craft exceptional
+                digital experiences."
+                className=""
+              />
+              <ToolboxItems items={toolboxItems} className="" />
+              <ToolboxItems
+                items={toolboxItems}
+                className="mt-6"
+                itemsWrapperClassName="-translate-x-1/2"
+              />
+            </Card>
           </div>
-        </Card>
-        <Card>
-          <div>
-            <StarIcon />
-            <h3>Beyone the Code</h3>
-            <p>Explore my interest and hobbies beyon the digital realm.</p>
+          <div className="grid grid-cols-1 
+          md:grid-cols-5 lg:grid-cols-3  gap-8">
+            <Card className="h-[320px] p-0 flex 
+            flex-col col-span-3 lg:col-span-2">
+              <CardHeader
+                title="Beyone the Code"
+                description="Explore my interest and hobbies beyon the digital realm."
+                className="px-6 py-6"
+              />
+              <div className="relative flex-1">
+                {hobbies.map(hobby => (
+                  <div key={hobby.title}
+                    className="inline-flex items-center gap-2 px-6 
+                bg-gradient-to-r from-emerald-300 
+                to-sky-400 rounded-full py-1.5 absolute"
+                    style={{
+                      left: hobby.left,
+                      top: hobby.top,
+                    }}
+                  >
+                    <span className="font-medium text-gray-950">{hobby.title}</span>
+                    <span>{hobby.emoji}</span>
+                  </div>
+                ))}
+              </div>
+            </Card>
+            <Card className="h-[320px] p-0 
+            relative col-span-2 lg:col-span-1">
+              <Image src={mapImage} alt='map'
+                className="h-full w-full object-cover object-left-top"
+              />
+              <div className="absolute top-1/2 left-1/2
+            -translate-x-1/2 -translate-y-1/2 size-20
+            rounded-full bg-gradient-to-r 
+            from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0
+            after:outline after:outline-2 after:-outline-offset-2 after:rounded-full
+            after:outline-gray-950/30">
+                <Image src={simleMemoji} alt="smiling memoji"
+                  className="size-20" />
+              </div>
+            </Card>
           </div>
-        </Card>
-        <Card>
-            <Image src={mapImage} alt='map' />
-            <Image src={simleMemoji} alt="smiling memoji" />
-        </Card>
+        </div>
       </div>
     </div>
   );
