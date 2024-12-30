f3342f2891189519f6e4f76a4b48add799164a09
Trang Vu <106600297+Trangvu801@users.noreply.github.com>
2024-12-29 23:51:23 -0500

    About Section Animations

--- starter-template/src/sections/About.tsx
+++ starter-template/src/sections/About.tsx
@@ -1,6 +1,6 @@
+'use client';
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
-import StarIcon from "@/assets/icons/star.svg"
 import bookImage from "@/assets/images/book-cover.png"
 import Image from "next/image";
 import JavascriptIcon from '@/assets/icons/square-js.svg';
@@ -9,11 +9,12 @@ import CssIcon from '@/assets/icons/css3.svg';
 import ReactIcon from '@/assets/icons/react.svg';
 import ChromeIcon from '@/assets/icons/chrome.svg';
 import GithubIcon from '@/assets/icons/github.svg';
-import { TechIcon } from "@/components/TechIcon";
 import mapImage from "@/assets/images/map.png"
 import simleMemoji from "@/assets/images/memoji-smile.png"
 import { CardHeader } from "@/components/CardHeader";
 import { ToolboxItems } from "@/components/ToolboxItems";
+import {motion} from "framer-motion";
+import { useRef } from "react";
 
 const toolboxItems = [
   {
@@ -88,6 +89,7 @@ const hobbies = [
 ]
 
 export const AboutSection = () => {
+  const constraintRef = useRef(null);
   return (
     <div className="py-20 lg:py-28">
       <div className="container">
@@ -114,12 +116,11 @@ export const AboutSection = () => {
                 digital experiences."
                 className=""
               />
-              <ToolboxItems items={toolboxItems} className="" />
+              <ToolboxItems items={toolboxItems} className="" itemsWrapperClassName="animate-move-left [animation-duration:30s]"/>
               <ToolboxItems
                 items={toolboxItems}
                 className="mt-6"
-                itemsWrapperClassName="-translate-x-1/2"
-              />
+                itemsWrapperClassName="animate-move-right [animation-duration:15s]"/>
             </Card>
           </div>
           <div className="grid grid-cols-1 
@@ -131,9 +132,10 @@ export const AboutSection = () => {
                 description="Explore my interest and hobbies beyon the digital realm."
                 className="px-6 py-6"
               />
-              <div className="relative flex-1">
+              <div className="relative flex-1" ref= {constraintRef} >
                 {hobbies.map(hobby => (
-                  <div key={hobby.title}
+                  <motion.div 
+                  key={hobby.title}
                     className="inline-flex items-center gap-2 px-6 
                 bg-gradient-to-r from-emerald-300 
                 to-sky-400 rounded-full py-1.5 absolute"
@@ -141,10 +143,12 @@ export const AboutSection = () => {
                       left: hobby.left,
                       top: hobby.top,
                     }}
+                    drag
+                    dragConstraints={constraintRef}
                   >
                     <span className="font-medium text-gray-950">{hobby.title}</span>
                     <span>{hobby.emoji}</span>
-                  </div>
+                  </motion.div>
                 ))}
               </div>
             </Card>
@@ -155,10 +159,13 @@ export const AboutSection = () => {
               />
               <div className="absolute top-1/2 left-1/2
             -translate-x-1/2 -translate-y-1/2 size-20
-            rounded-full bg-gradient-to-r 
-            from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0
+            rounded-full after:content-[''] after:absolute after:inset-0
             after:outline after:outline-2 after:-outline-offset-2 after:rounded-full
             after:outline-gray-950/30">
+              <div className="absolute inset-0 rounded-full bg-gradient-to-r 
+            from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s]"></div>
+              <div className="absolute inset-0 rounded-full bg-gradient-to-r 
+            from-emerald-300 to-sky-400 -z-10"></div>
                 <Image src={simleMemoji} alt="smiling memoji"
                   className="size-20" />
               </div>
--- starter-template/tailwind.config.ts
+++ starter-template/tailwind.config.ts
@@ -24,13 +24,14 @@ const config: Config = {
       },
     },
     extend: {
-      fontFamily:{
+      fontFamily: {
         sans: 'var(--font-sans)',
         serif: 'var(--font-serif)',
       },
       animation: {
         'ping-large': "ping-large 1s ease-in-out infinite",
         'move-left': "move-left 1s linear infinite",
+        'move-right': "move-right 1s linear infinite",
       },
       keyframes: {
         'ping-large': {
@@ -45,8 +46,16 @@ const config: Config = {
           },
           '100%': {
             transform: 'translateX(-50%)'
-          }
-        }
+          },
+        },
+        'move-right': {
+          '0%': {
+            transform: 'translateX(-50%)'
+          },
+          '100%': {
+            transform: 'translateX(0%)'
+          },
+        },
       },
     },
   },
