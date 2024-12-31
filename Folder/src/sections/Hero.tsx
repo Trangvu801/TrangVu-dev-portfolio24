d16191bf5751c997492a803e0987875c21b8d033
Trang Vu <106600297+Trangvu801@users.noreply.github.com>
2024-12-30 23:09:15 -0500

    Update Hero.tsx

--- Folder/src/sections/Hero.tsx
+++ Folder/src/sections/Hero.tsx
@@ -63,10 +63,10 @@ export const HeroSection = () => {
           </div>
         </div>
         <div className='max-w-lg mx-auto'>
-          <h1 className='font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide'>Building Exceptional User Experiences</h1>
           <p className='mt-4 text-center text-white/60 md:text-lg'>
-            Hi I am Trang Vu, a Front-End Developer and Technical Project Manager based in Pennsylvania, USA.
+            Hi I&#39;m Trang Vu, a Front-End Developer and Technical Project Manager based in Pennsylvania, USA.
           </p>
+
 
         </div>
         <div className='flex flex-col md:flex-row justify-center items-center mt-8 gap-4'>
